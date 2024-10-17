e5e231f2ecfad9c8ff073ea2e8e942fe2fc9ef09
Raoul1808 <raoulthegeek@gmail.com>
2024-10-17 16:28:35 +0200

    Add custom death link comment

--- worlds/srxd/Options.py
+++ worlds/srxd/Options.py
@@ -10,7 +10,12 @@ from Options import PerGameCommonOptions, DeathLink, OptionSet
 #     valid_keys = ["Supporter Pack", "Monstercat DLC", "Chillhop DLC"]
 
 
+class SrxdDeathLink(DeathLink):
+    """When you die, everyone who enabled death link dies. Of course, the reverse is true too.
+    This option can also be toggled in-game."""
+
+
 @dataclass
 class SrxdOptions(PerGameCommonOptions):
-    death_link: DeathLink
+    death_link: SrxdDeathLink
 #     enabled_dlc: SrxdDLC
