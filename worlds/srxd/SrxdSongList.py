3aed6afa64119b04e8873da3ed3c5d1cf498a5c7
Raoul1808 <raoulthegeek@gmail.com>
2024-10-24 15:57:20 +0200

    Move starting songs and boss song variables

--- worlds/srxd/SrxdSongList.py
+++ worlds/srxd/SrxdSongList.py
@@ -1,5 +1,3 @@
-import random
-
 from typing import Dict
 
 
@@ -10,21 +8,13 @@ def load_text_file(name: str) -> str:
 
 class SrxdSongList:
     songs: Dict[int, str]
-    starting_songs: [int]
-    boss_song: int
 
     def __init__(self):
-        songs = []
-        starting_songs = []
+        songs = {}
         songs_file = load_text_file("SongList.txt")
         for line in songs_file.splitlines():
             elems = line.split('|')
             song_id = int(elems[0])
             song_name = elems[1]
-            songs.append((song_id, song_name))
-        song_indices = list(range(1, len(songs) + 1))
-        for _ in range(3):
-            starting_songs.append(song_indices.pop(random.randint(0, len(song_indices) - 1)))
-        self.boss_song = starting_songs.pop(random.randint(0, len(starting_songs) - 1))
-        SrxdSongList.songs = songs
-        SrxdSongList.starting_songs = starting_songs
+            songs[song_id] = song_name
+        self.songs = songs
--- worlds/srxd/__init__.py
+++ worlds/srxd/__init__.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from BaseClasses import Tutorial, Region, ItemClassification
 from worlds.AutoWorld import WebWorld, World
 from .Items import SrxdItem
@@ -28,14 +30,21 @@ class SrxdWorld(World):
     options_dataclass = SrxdOptions
 
     song_list = SrxdSongList()
+    boss_song: int
+    starting_songs: [int]
 
-    item_name_to_id = {name: code for code, name in song_list.songs}
-    location_name_to_id = {name: code for code, name in song_list.songs}
+    item_name_to_id = {name: code for code, name in song_list.songs.items()}
+    location_name_to_id = {name: code for code, name in song_list.songs.items()}
 
     def generate_early(self) -> None:
-        for song_id in self.song_list.starting_songs:
-            _, name = self.song_list.songs[song_id - 1]
-            self.multiworld.push_precollected(self.create_item(name))
+        song_ids = [song_id for song_id, _ in self.song_list.songs.items()]
+        self.starting_songs = []
+        for _ in range(3):
+            song_id = song_ids.pop(randint(0, len(song_ids) - 1))
+            song_name = self.song_list.songs[song_id]
+            self.starting_songs.append(song_id)
+            self.multiworld.push_precollected(self.create_item(song_name))
+        self.boss_song = song_ids.pop(randint(0, len(song_ids) - 1))
 
     def create_item(self, name: str) -> SrxdItem:
         return SrxdItem(name, ItemClassification.progression, self.item_name_to_id[name], self.player)
@@ -43,14 +52,14 @@ class SrxdWorld(World):
     def create_regions(self) -> None:
         menu_region = Region("Menu", self.player, self.multiworld)
         self.multiworld.regions.append(menu_region)
-        for song_id, song_name in self.song_list.songs:
+        for song_id, song_name in self.song_list.songs.items():
             loc = SrxdLocation(self.player, song_name, self.location_name_to_id[song_name], menu_region)
             loc.access_rule = lambda state, place=song_name: state.has(place, self.player)
             menu_region.locations.append(loc)
 
     def create_items(self) -> None:
-        for song_id, song_name in self.song_list.songs:
-            if song_id != self.song_list.boss_song:
+        for song_id, song_name in self.song_list.songs.items():
+            if song_id != self.boss_song and song_id not in self.starting_songs:
                 self.multiworld.itempool.append(self.create_item(song_name))
 
     def fill_slot_data(self):
@@ -59,6 +68,6 @@ class SrxdWorld(World):
             "clearCondition": self.options.clear_condition.value,
             "medalRequirement": self.options.medal_requirement.value,
             "targetAccuracy": self.options.target_accuracy.value,
-            "bossSong": self.song_list.boss_song,
+            "bossSong": self.boss_song,
             "clearsRequiredForGoal": self.options.goal_requirement.value,
         }
